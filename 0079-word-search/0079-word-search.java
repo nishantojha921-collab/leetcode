df0235f7feb622c6f3ec9250465998de394cc4d0
nishantojha921-collab <nishantojha921@gmail.com>
2026-08-03 22:55:47 +0530

    Time: 113 ms (94.28%), Space: 43 MB (60.93%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean exists(char[][] board, int i,int j,char[] word,int idx){
+        if(i<0 || i>=board.length ||j<0 || j>=board[0].length || board[i][j] == '*'|| board[i][j] != word[idx] ) return false;
+        if(idx == word.length-1) return true;
+
+        char ch=board[i][j];
+        board[i][j] = '*';
+        boolean res = exists(board,i+1,j,word, idx+1)||
+        exists(board,i-1,j,word, idx+1)||
+        exists(board,i,j+1,word, idx+1)||
+        exists(board,i,j-1,word, idx+1);
+        board[i][j] = ch;
+        return res;
+    }
+    public boolean exist(char[][] board, String word) {
+        char[] word_arr = word.toCharArray();
+
+        for(int i =0;i<board.length;i++){
+            for(int j= 0;j<board[0].length;j++){
+                if(board[i][j] == word_arr[0] && exists(board,i,j,word_arr,0)) return true;
+            }
+        }
+        return false;
+    }
+}
