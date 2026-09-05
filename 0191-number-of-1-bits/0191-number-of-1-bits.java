70923ebf60fede08fd6d0ad550fd201580a8a38c
nishantojha921-collab <nishantojha921@gmail.com>
2026-09-06 00:03:41 +0530

    Time: 1 ms (29.86%), Space: 42.6 MB (27.9%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -3,8 +3,10 @@ class Solution {
         int count = 0;
 
         while(n != 0){
-            count += ( n & 1);
-            n = n >>> 1;
+            // count += ( n & 1);
+            // n = n>>>1;
+            n = n & (n - 1);
+            count++; 
         }
         return count;
     }
